df7a78353921ecca5a6e69ece64d9fe9db125115
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2025-04-06 20:56:37 +0530

    fix(auth-client): improve base URL handling and logging
    - Updated the auth client to use a fallback base URL if NEXT_PUBLIC_API_URL is not set, enhancing robustness.
    - Added logging for the base URL to aid in debugging and configuration verification.

--- apps/web/lib/auth-client.tsx
+++ apps/web/lib/auth-client.tsx
@@ -3,10 +3,14 @@ import { apiKeyClient } from "better-auth/client/plugins"
 
 if (!process.env.NEXT_PUBLIC_API_URL) {
   console.log('Warning: NEXT_PUBLIC_API_URL environment variable is not set')
+} else {
+    console.log("NEXT_PUBLIC_API_URL")
 }
 
+const baseUrl = process.env.NEXT_PUBLIC_API_URL || "https://api.sector-iot.space"
+
 export const authClient =  createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_API_URL, // the base url of your auth server 
+    baseURL: baseUrl, // the base url of your auth server 
     plugins: [ 
         apiKeyClient() 
     ]
